050f2bcad1afb7b897c2754d0d20ab30b1fff92f
hojkas <ivka.strnadova@gmail.com>
2019-11-27 19:28:25 +0100

    parser begin

---
+++ lib/parser.h
@@ -0,0 +1,20 @@
+/** @parser.h
+ * Syntakticka analyza rekurzivnim sestupem vseho krom vyrazu
+ *
+ * @author xstrna14
+ * @version 1.0
+ * @date 27.11.2019
+ */
+
+ #ifndef _PARSER_H_
+ #define _PARSER_H_
+
+#include "../scanner/scanner.h"
+#include "../scanner/token.h"
+#include "symtable.h"
+#include "dynamic_string.h"
+#include "errors.h"
+#include "adress_code.h"
+//#include ""
+
+ #endif //_PARSER_H_
