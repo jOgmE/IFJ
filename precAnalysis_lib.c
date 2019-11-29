af26a60bc92af68771854080cf9d0037a75a930b
Jaroslav Hort <hortjaroslav0@gmail.com>
2019-11-29 22:59:11 +0100

    Fixed Pepega

--- precAnalysis_lib.c
+++ precAnalysis_lib.c
@@ -1,59 +1,67 @@
-#include "precAnalysis.h"
+#include "precAnalysis_lib.h"
 
-Token *expressionAnalysis ( Token *input1, Token *input2, Token *res ) {
+Token *expressionAnalysis(Token *input1, Token *input2, Token *res)
+{
 
 	PAStack *socket;
 	PAInit(&socket);
 
-	if ( input2 != NULL ) PAPush(socket, input2);
-	PAPush(socket, input1);	
+	if (input2 != NULL)
+		PAPush(socket, input2);
+	PAPush(socket, input1);
 
 	PAStack *s;
 	PAInit(&s);
 	PAPushFin(s);
 
-	Token *curToken = ( input2 == NULL ) ? input1 : input2;
+	Token *curToken = (input2 == NULL) ? input1 : input2;
 
 	int *isFin;
 	*isFin = 0;
 
-	do {
+	do
+	{
 
+		switch (getFromTable(PATopTerm(s)->type, curToken->type, isFin))
+		{
 
-		switch (getFromTable(PATopTerm(s)->type, curToken->type, isFin)) {
+		case '[':
+			PAPush(s, curToken);
+			if (PATop(socket) != NULL)
+			{
+				curToken = PATop(socket);
+				PAPop(socket);
+			}
+			else
+			{
+				getToken(curToken);
+			}
+			break;
 
-			case '[':
-				PAPush(s, curToken);
-				if (PATop(socket) != NULL) {
-					curToken = PATop(socket);
-					PAPop(socket);	
-				} else {
-					getToken(curToken);
-				}
-				break;
+		case '=':
+			PAAddBracket(s);
+			PAPush(s, curToken);
+			if (PATop(socket) != NULL)
+			{
+				curToken = PATop(socket);
+				PAPop(socket);
+			}
+			else
+			{
+				getToken(curToken);
+			}
+			break;
 
-			case '=':
-				PAAddBracket(s);
-				PAPush(s, curToken);
-				if (PATop(socket) != NULL) {
-					curToken = PATop(socket);
-					PAPop(socket);	
-				} else {
-					getToken(curToken);
-				}
-				break;
+		case ']':
+			PAApplyRule(s, res); // <- tady je moloch
+			break;
 
-			case ']':
-				PAApplyRule(s, res);	// <- tady je moloch
-				break;
+		default:
+			// TODO error
+			break;
+		}
 
-			default:
-				// TODO error
-				break;
-
-		
-
-	} while ( PAEndAtTop(s) || !(*isFin) );
+	} while (PAEndAtTop(s) || !(*isFin));
 
 	PAYeet(socket);
 	PAYeet(s);
