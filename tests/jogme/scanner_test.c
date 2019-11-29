44aa0d63607417f73ae349bdfda6552feffbc326
Jaroslav Hort <hortjaroslav0@gmail.com>
2019-11-29 20:54:09 +0100

    Revert "minor fixes"
    This reverts commit 0d9c1878a5a5abf521a46a31fb887bae2ebec909.

--- tests/jogme/scanner_test.c
+++ tests/jogme/scanner_test.c
@@ -6,8 +6,8 @@
 
 #include "../../lib/dynamic_string.h"
 #include "../../lib/errors.h"
-#include "../../lib/token.h"
-#include "../../lib/scanner.h"
+#include "../../scanner/token.h"
+#include "../../scanner/scanner.h"
 
 //global variable for file
 FILE *f;
