ce48b21f6af3ee5df8a979dae732131109ee67e4
xdrabe09 <draberf@gmail.com>
2019-12-06 02:09:48 +0100

    Added comments to PA-related files.

--- precAnalysis_lib.c
+++ precAnalysis_lib.c
@@ -1,8 +1,14 @@
 #include "precAnalysis_lib.h"
 
+// struktura prec analyzy je v podstate totozna s jejim popisem v prezentaci
+// (dokonce i zasobnik pouziva znakove reprezentace 'E', 'i', '+' za operatory, '$' atd)
+// Vetsina funkcionality se ukryva ve funkci PAApplyRule(), kterou nelze prehlednout
+// v "precAnalysis_lib.h"
+
 Token *expressionAnalysis(Token *input1, Token *input2, Token *res)
 {
 
+	// socket stack pro akomodaci vice prichazejicich vstupu
 	PAStack *socket;
 	PAInit(&socket);
 
@@ -10,6 +16,7 @@ Token *expressionAnalysis(Token *input1, Token *input2, Token *res)
 		PAPush(socket, input2);
 	PAPush(socket, input1);
 
+	// inicializace hlavniho stacku PA
 	PAStack *s;
 	PAInit(&s);
 	PAPushFin(s);
@@ -21,6 +28,7 @@ Token *expressionAnalysis(Token *input1, Token *input2, Token *res)
 
 
 	do {
+		// F L U S H
 		if (kill_after_analysis == true) {
 			
 			while (decode(curToken->type)!=17)
@@ -37,7 +45,7 @@ Token *expressionAnalysis(Token *input1, Token *input2, Token *res)
 		}
 
 
-
+		// vybere z tabulky PA prislusny symbol
 		char switchC = getFromTable(PATopTerm(s)->type, curToken->type, isFin);
 
 		switch (switchC) {
@@ -67,6 +75,8 @@ Token *expressionAnalysis(Token *input1, Token *input2, Token *res)
 				PAApplyRule(s, res);	// <- tady je moloch
 				break;
 
+			// kdyby scanner narazil na token mimo rozsah PA, je interpretovan jako '$', ktery ma sve ']'
+			// a tim padem nemusi vylozene prechazet na chybu
 			default:
 				kill_after_analysis = true;
 				global_error_code = SYNTAX_ANALYSIS_ERROR;
@@ -77,9 +87,10 @@ Token *expressionAnalysis(Token *input1, Token *input2, Token *res)
 
 	} while ( PAEndAtTop(s) || !(*isFin) );
 
-
+	// uvolneni
 	PAYeet(socket);
 	PAYeet(s);
 
+	// navrat navic prejateho tokenu (interpretovaneho jako '$')
 	return curToken;
 }
